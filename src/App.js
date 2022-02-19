d16e90d3654b8fb52be9e0a45e2dc78cdd073d9e
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-19 17:43:20 +0100

    Added buttons with bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useRef} from 'react'
 import Webcam from 'react-webcam';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -17,44 +19,40 @@ function App() {
     <header className="App-header">
       <Webcam mirrored={true}
         ref={webcamRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zindex: 9,
-          width: 640,
-          height: 480,
-        }}
+        // style={{
+        //   position: "absolute",
+        //   marginLeft: "auto",
+        //   marginRight: "auto",
+        //   left: 0,
+        //   right: 0,
+        //   textAlign: "center",
+        //   zindex: 9,
+        //   width: 640,
+        //   height: 480,
+        // }}
       />
 
       <canvas
         ref={canvasRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zindex: 9,
-          width: 640,
-          height: 480,
-        }}
+        // style={{
+        //   position: "absolute",
+        //   marginLeft: "auto",
+        //   marginRight: "auto",
+        //   left: 0,
+        //   right: 0,
+        //   textAlign: "center",
+        //   zindex: 9,
+        //   width: 640,
+        //   height: 480,
+        // }}
       />
-      {/* <button style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 60,
-          right: 0,
-          textAlign: "right",
-          zindex: 9,
-          width: 50,
-          height: 50,
-        }}>Default</button>; */}
+      <Button variant="primary">Primary</Button>{' '}
+    <Button variant="secondary">Secondary</Button>{' '}
+    <Button variant="success">Success</Button>{' '}
+    <Button variant="warning">Warning</Button>{' '}
+    <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+    <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+    <Button variant="link">Link</Button>
     </header>
   </div>
   
