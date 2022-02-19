339fd253efa0545e6ff6e2bbd9a4d6040493116b
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-19 17:28:06 +0100

    Replaced default app and added the webcam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react'
+import Webcam from 'react-webcam';
 import './App.css';
 
+
+
+
+
+
+
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <Webcam mirrored={true}
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+      {/* <button style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 60,
+          right: 0,
+          textAlign: "right",
+          zindex: 9,
+          width: 50,
+          height: 50,
+        }}>Default</button>; */}
+    </header>
+  </div>
+  
   );
 }
 
