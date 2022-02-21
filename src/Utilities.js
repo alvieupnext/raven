cd6ff880e4c683b3aa35c026c21bf4029f891e0e
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-21 11:24:36 +0100

    Added comments to Utilities

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,13 +1,17 @@
-// logs a message to the application
+// logs a message to the application and also logs it to the console
 function logToApp(message) {
     document.getElementById("appLog").innerHTML = message
+    console.log(message)
   };
 
+
+//sets an attribute to a new value and returns the json
 function setJSON(json, att, value){
     json[att] = value;
     return json;
  }
 
+ //dereferences a json, creating a clone of a json object
  function dereference(json) {
     // return json
     return {...json}
