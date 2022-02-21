ae2ad786499edbf9101e88e21ef69c2cbcaf7bb8
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-21 11:39:04 +0100

    Added two new export options

--- src/exports.js
+++ src/exports.js
@@ -1,48 +1,73 @@
-import {map} from 'rxjs';
-import {setJSON, dereference} from './Utilities'
+import { map } from 'rxjs';
+import { setJSON, dereference } from './Utilities'
 
-function vanillaExport(observer){
-    return observer
- }
- 
- function minimalExport(observer){
-    return observer
-    .pipe(map(json => {delete json.origin; return json}))
- }
- 
- function timestampExport(observer){
-    return observer
-    .pipe(map(json => setJSON(json, 'timestamp', Date.now())))
- }
- 
- function historyExport(observer){
-    function exportFunction(json){
-       let history = json.history
-       if (history === undefined) {
-          history = []
-       }
-       delete json.history
-       let copy = dereference(json) 
-       
-       history.push(copy)
-       
-       json['history'] = history;
-       return json
-    }
-    return observer
-    .pipe(map(exportFunction))
- }
- 
- function totalDelayExport(observer){
-   function difference(json){
-     if (json.history === undefined){
-       return 0
-     }
-     else return json['timestamp'] - json['history'][0].timestamp
+function vanillaExport(observer) {
+   return observer
+}
+
+function minimalExport(observer) {
+   return observer
+      .pipe(map(json => { delete json.origin; return json }))
+}
+
+function timestampExport(observer) {
+   return observer
+      .pipe(map(json => setJSON(json, 'timestamp', Date.now())))
+}
+
+function historyExport(observer) {
+   function exportFunction(json) {
+      let history = json.history
+      if (history === undefined) {
+         history = []
+      }
+      delete json.history
+      let copy = dereference(json)
+
+      history.push(copy)
+
+      json['history'] = history;
+      return json
    }
    return observer
-   .pipe(map(json => setJSON(json, 'totalDelay', difference(json))))
- }
+      .pipe(map(exportFunction))
+}
 
- // eslint-disable-next-line import/no-anonymous-default-export
-export default {vanillaExport, minimalExport, totalDelayExport, historyExport, timestampExport}
+function totalDelayExport(observer) {
+   function difference(json) {
+      if (json.history === undefined) {
+         return 0
+      }
+      else return json['timestamp'] - json['history'][0].timestamp
+   }
+   return observer
+      .pipe(map(json => setJSON(json, 'totalDelay', difference(json))))
+}
+
+
+function delayExport(observer) {
+   function difference(json) {
+      if (json.history === undefined) {
+         return 0
+      }
+      else {
+         let lastIndex = json.history.length - 1
+         return json['timestamp'] - json['history'][lastIndex].timestamp
+      }
+   }
+   return observer
+      .pipe(map(json => setJSON(json, 'delay', difference(json))))
+}
+
+function completeExport(observer) {
+   return observer
+      .pipe(timestampExport,
+         delayExport,
+         totalDelayExport,
+         historyExport,
+      )
+}
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { vanillaExport, minimalExport, totalDelayExport, historyExport, timestampExport, delayExport, completeExport }
