aebba21b97a03f524d91ef89ceb609e4d162f7e1
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-21 11:58:22 +0100

    Added new procedures to Utilities

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,25 +1,34 @@
+const refreshRate = 60
+
 // logs a message to the application and also logs it to the console
 function logToApp(message) {
     document.getElementById("appLog").innerHTML = message
     console.log(message)
-  };
+};
 
 
 //sets an attribute to a new value and returns the json
-function setJSON(json, att, value){
+function setJSON(json, att, value) {
     json[att] = value;
     return json;
- }
+}
 
- //dereferences a json, creating a clone of a json object
- function dereference(json) {
+//dereferences a json, creating a clone of a json object
+function dereference(json) {
     // return json
-    return {...json}
-  }
+    return { ...json }
+}
 
-const refreshRate = 60
- 
+function setOrigin(json, origin) {
+    json['origin'] = origin
+    return json
+}
+
+function transformValue(json, transform) {
+    return setJSON(json, 'value', transform(json['value']))
+}
+
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export {logToApp, setJSON, dereference, refreshRate}
+export { logToApp, setJSON, dereference, refreshRate, setOrigin, transformValue }
