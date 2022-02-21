a4d7eb4612f72cca4583403f6f206efdf0962d12
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-21 11:23:28 +0100

    Added setJSON and dereference to Utilities

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,6 +1,19 @@
+// logs a message to the application
 function logToApp(message) {
     document.getElementById("appLog").innerHTML = message
   };
 
+function setJSON(json, att, value){
+    json[att] = value;
+    return json;
+ }
 
-export default logToApp
+ function dereference(json) {
+    // return json
+    return {...json}
+  }
+ 
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {logToApp, setJSON, dereference}
