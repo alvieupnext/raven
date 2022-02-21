2def9d861212ccc671e4d51d1ce3857230631554
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-21 11:26:00 +0100

    Added exports.js

---
+++ src/exports.js
@@ -0,0 +1,48 @@
+import {map} from 'rxjs';
+import {setJSON, dereference} from './exports'
+
+function vanillaExport(observer){
+    return observer
+ }
+ 
+ function minimalExport(observer){
+    return observer
+    .pipe(map(json => {delete json.origin; return json}))
+ }
+ 
+ function timestampExport(observer){
+    return observer
+    .pipe(map(json => setJSON(json, 'timestamp', Date.now())))
+ }
+ 
+ function historyExport(observer){
+    function exportFunction(json){
+       let history = json.history
+       if (history === undefined) {
+          history = []
+       }
+       delete json.history
+       let copy = dereference(json) 
+       
+       history.push(copy)
+       
+       json['history'] = history;
+       return json
+    }
+    return observer
+    .pipe(map(exportFunction))
+ }
+ 
+ function totalDelayExport(observer){
+   function difference(json){
+     if (json.history === undefined){
+       return 0
+     }
+     else return json['timestamp'] - json['history'][0].timestamp
+   }
+   return observer
+   .pipe(map(json => setJSON(json, 'totalDelay', difference(json))))
+ }
+
+ // eslint-disable-next-line import/no-anonymous-default-export
+export default {vanillaExport, minimalExport, totalDelayExport, historyExport, timestampExport}
