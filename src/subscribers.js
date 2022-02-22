2a2d6a49c7f9f0f2dded3002c165101e8f41ff47
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-22 15:54:14 +0100

    Added consoleSubscriber

--- src/subscribers.js
+++ src/subscribers.js
@@ -3,9 +3,17 @@
 import { commandLog } from "./Utilities";
 
 
+let consoleSubscriber = {
+    next: (data) => { console.log(data);},
+    error: (error) => { console.log(error) },
+    complete: () => { console.log('Completed') }
+}
+
 
 let logSubscriber = {
     next: (data) => { console.log(data); commandLog(data.value);},
     error: (error) => { console.log(error) },
     complete: () => { console.log('Completed') }
 }
+
+export {consoleSubscriber, logSubscriber}
