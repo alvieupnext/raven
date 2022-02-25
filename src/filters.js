9dfca47c9ef858282aba408f3bfc9681f40e8caf
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-25 22:24:27 +0100

    and made the code prettier in filters.js

--- src/filters.js
+++ src/filters.js
@@ -1,58 +1,58 @@
 import { dereference, setJSON, transformValue } from "./Utilities";
-import {map, filter} from "rxjs"
+import { map, filter } from "rxjs"
 
-function createFilterer(filterf){
-    return (observable) => {
-      return observable
+function createFilterer(filterf) {
+  return (observable) => {
+    return observable
       .pipe(filter(data => filterf(data)))
-    }
   }
+}
 
-function sortGestures(values){
-    let index = 0;
-    for (let valu of values){
-      let clone = dereference(valu)
-      values[index] = setJSON(clone, 'gesture', setJSON(clone.gesture, 'gestures', clone.gesture.gestures.sort(compareFPGestures)))
-      index++;
-    }
-    return values
+function sortGestures(values) {
+  let index = 0;
+  for (let valu of values) {
+    let clone = dereference(valu)
+    values[index] = setJSON(clone, 'gesture', setJSON(clone.gesture, 'gestures', clone.gesture.gestures.sort(compareFPGestures)))
+    index++;
   }
-  
-  
-  
-  function createSort(sortf){ //requires data to be an array
-    return (observable) => {
-      return observable
+  return values
+}
+
+
+
+function createSort(sortf) { //requires data to be an array
+  return (observable) => {
+    return observable
       // .pipe(tap(json => console.log(json.value)))
       .pipe(map(json => transformValue(json, sortf)))
-      // .pipe(tap(json => console.log(json.value)))
-    }
+    // .pipe(tap(json => console.log(json.value)))
   }
-  
-  function compareFPGestures(a, b){ //get best feature first
-    if (a.score === b.score){
-      return 0;
-    }
-    return a.score > b.score ? -1 : 1
-  }
-  
-  function removeEmptyHand(json){
-    return json.value.multiHandWorldLandmarks.length !== 0
-  }
-  
-  function removeNoGestures(json){
-    for (let gest of json.value){
-      if (gest.gesture !== "no_gesture"){
-        return true
-      }
-    }
-    return false
-  }
+}
 
-  const SortByBestGesture = createSort(sortGestures)
-  
+function compareFPGestures(a, b) { //get best feature first
+  if (a.score === b.score) {
+    return 0;
+  }
+  return a.score > b.score ? -1 : 1
+}
+
+function removeEmptyHand(json) {
+  return json.value.multiHandWorldLandmarks.length !== 0
+}
+
+function removeNoGestures(json) {
+  for (let gest of json.value) {
+    if (gest.gesture !== "no_gesture") {
+      return true
+    }
+  }
+  return false
+}
+
+const SortByBestGesture = createSort(sortGestures)
+
 
 
 const EmptyHandFilter = createFilterer(removeEmptyHand)
 
-export {EmptyHandFilter, SortByBestGesture}
+export { EmptyHandFilter, SortByBestGesture }
