59acf176ed5705b7d74acab7d18b48dfc7752edf
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-26 19:38:13 +0100

    Added EmptyArrayFilter to Raven

--- src/raven.js
+++ src/raven.js
@@ -1,7 +1,7 @@
 import { webcamStream, primeStream, mediapipeStream, loadModel, fingerposeStream, gesturer, commandStream, bufferStream, frequencyStream} from './streams.js';
 import {setExporter, completeExport} from './exports'
 import { consoleSubscriber, logSubscriber } from './subscribers';
-import { EmptyHandFilter } from './filters';
+import { EmptyArrayFilter, EmptyHandFilter } from './filters';
 
 // setExporter(completeExport)
 
@@ -14,7 +14,8 @@ function createRaven(webcamRef, canvasRef){
     gesturer,
     commandStream,
     bufferStream(1000),
-    frequencyStream
+    EmptyArrayFilter,
+    frequencyStream,
     )
 }
 
