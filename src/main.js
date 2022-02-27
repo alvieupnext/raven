b46e9131f3472425da093a115ffeeaf8fe90a2ab
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-27 01:25:25 +0100

    Trying out tello drone on the main module

--- src/main.js
+++ src/main.js
@@ -30,6 +30,16 @@ function createWindow () {
   win.webContents.openDevTools()
 }
 
+const tello = new TelloDrone()
+
+tello.start()
+
+function sendToTello(commands){
+  return "hi"
+}
+
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
