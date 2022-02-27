53aa706921a4303a336d4927c4bba9cdb169e123
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-27 14:00:43 +0100

    Implemented takeoff sequence

--- src/drone.js
+++ src/drone.js
@@ -1,41 +1,56 @@
 
 //drone module which takes care of communications with drone
 
-const { logDroneHistory } = require("./Utilities");
+const { logDroneHistory, logToApp } = require("./Utilities");
 
 let history = []
 
 let takeoff = false
 
 let ws = new WebSocket('ws://localhost:4000/ws')
-ws.onopen = function() {
-                
-  // Web Socket is connected, send data using send()
-  console.log("Server is Open");
-};
-ws.onmessage = function (evt) { 
-var received_msg = evt.data;
-console.log("Message is received..." + received_msg);
-};
+ws.onopen = function () {
 
-ws.onclose = function() { 
-
-// websocket is closed.
-console.log("Connection is closed..."); 
+    // Web Socket is connected, send data using send()
+    console.log("Server is Open");
+};
+ws.onmessage = function (evt) {
+    var received_msg = evt.data;
+    console.log("Message is received..." + received_msg);
 };
 
-function processCommand(command){
+ws.onclose = function () {
+
+    // websocket is closed.
+    console.log("Connection is closed...");
+};
+
+//could also be changed to 5
+let counter = 3;
+
+function processCommand(command) {
     history.push(command)
+    if (!takeoff) {
+        if (typeof command.name === 'number' && counter === command.name) { //takeoff sequence?
+            logToApp(counter, "appLog")
+            counter -= 1
+            if (counter === 0) {
+                history.push({ name: 'takeoff' })
+                takeoff = true
+                counter = 3
+            }
+        }
+        else { counter = 3 }
+    }
 }
 
 
 
-function sendToDrone(commands){
-    if (commands.length === 1){ //just send the command to the drone
+function sendToDrone(commands) {
+    if (commands.length === 1) { //just send the command to the drone
         processCommand(commands[0])
         logDroneHistory(history)
     }
 
 }
 
-export {sendToDrone}
+export { sendToDrone }
