d4468213a20910dbc1c16c445bfe9a309fd0baa6
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-27 14:17:17 +0100

    implementing early version of server

--- src/telloserver.js
+++ src/telloserver.js
@@ -2,6 +2,10 @@ const { TelloDrone } = require('yatsw')
 
 const tello = new TelloDrone()
 
+let ws = new WebSocket.Server({port:4000})
+
+// ws.start()
+
 tello.start()
 
 function sendToTello(commands){
