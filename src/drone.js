134d60abcf78e40439bfd9b011fc68e86b7b329a
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-02-27 13:37:12 +0100

    websocket

--- src/drone.js
+++ src/drone.js
@@ -1,12 +1,36 @@
-// const { TelloDrone } = require('yatsw')
 
-// const Tello = new TelloDrone()
-// Tello.start()
+//drone module which takes care of communications with drone
 
-function sendToTello(commands){
+const { logDroneHistory } = require("./Utilities");
+
+let history = []
+
+let ws = new WebSocket('ws://localhost:4000/ws')
+ws.onopen = function() {
+                
+  // Web Socket is connected, send data using send()
+  console.log("Server is Open");
+};
+ws.onmessage = function (evt) { 
+var received_msg = evt.data;
+console.log("Message is received..." + received_msg);
+};
+
+ws.onclose = function() { 
+
+// websocket is closed.
+console.log("Connection is closed..."); 
+};
+
+
+
+function sendToDrone(commands){
+    if (commands.length === 1){ //just send the command to the drone
+        // ws.send(commands[0])
+        history.push(commands[0])
+        logDroneHistory(history)
+    }
 
 }
 
-
-
-export {sendToTello}
+export {sendToDrone}
