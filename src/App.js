c30f410fc89d31b8268a5decd999c75aa2830cb1
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-03-05 21:53:46 +0100

    Clear canvas on stopping stream

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ function App() {
     }
     else {
       stopRaven(raven.current)
+      let ctx = canvasRef.current.getContext("2d")
+      ctx.clearRect(0, 0, 640, 480)
       setButtonText("Resume")
     }
     running.current = !running.current
