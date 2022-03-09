995ce41459733111c1834791af7c88c4d0ef6dbd
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-03-09 16:55:50 +0100

    Tello server code

--- src/telloserver.js
+++ src/telloserver.js
@@ -1,63 +1,90 @@
-// const express = require('express');
-// const http = require('http');
-// const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
-// const app = express();
+const app = express();
 
-// //initialize a simple http server
-// const server = http.createServer(app);
+//initialize a simple http server
+const server = http.createServer(app);
 
-// //initialize the WebSocket server instance
-// const wss = new WebSocket.Server({ server });
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
 
+const { TelloDrone } = require('yatsw')
+
+const argdict = {
+    left: [1],
+    right: [1],
+}
+
+wss.on('connection', (ws) => {
+
+    const Tello = new TelloDrone()
+
+    Tello.start()
+
+    //events
+    Tello.on(Tello.events.MESSAGE, data => {
+        console.log(data)
+    })
+
+    //telemetry
+    Tello.on(Tello.events.STATE, data => {
+        console.log(JSON.parse(data))
+    })
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        // console.log(message.toString())
+        const index = message.indexOf('_')
+        if (index === -1){ //no parameter
+            let func = Tello[message]
+            func()
+        }
+        else {
+            let name = message.substring(0, index)
+            let func = Tello[name]
+            let arg = message.substring(index + 1)
+            console.log(arg)
+            console.log(name)
+            if (name === "flip"){
+                func(arg)
+            } 
+            else {
+                func(parseInt(arg))
+            }
+        }
+        const func = Tello[message.toString()]
+        // const arg = argdict[message.toString()]
+        // console.log(func)
+        // if (arg !== undefined){
+        //     func.apply(null, arg)
+        // }
+        // else {func()}
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+//start our server
+server.listen(4000, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
 // const { TelloDrone } = require('yatsw')
 
-// const argdict = {
-//     left: [1],
-//     right: [1],
+// const tello = new TelloDrone()
+
+// let ws = new WebSocket.Server({port:4000})
+
+// // ws.start()
+
+// tello.start()
+
+// function sendToTello(commands){
+//   return "hi"
 // }
-
-// wss.on('connection', (ws) => {
-
-//     const Tello = new TelloDrone()
-
-//     Tello.start()
-
-//     //connection is up, let's add a simple simple event
-//     ws.on('message', (message) => {
-
-//         //log the received message and send it back to the client
-//         console.log('received: %s', message);
-//         console.log(message)
-//         console.log(message.toString())
-//         const func = Tello[message.toString()]
-//         const arg = argdict[message.toString()]
-//         console.log(func)
-//         if (arg !== undefined){
-//             func.apply(null, arg)
-//         }
-//         else {func()}
-//         ws.send(`Hello, you sent -> ${message}`);
-//     });
-
-//     //send immediatly a feedback to the incoming connection    
-//     ws.send('Hi there, I am a WebSocket server');
-// });
-
-// //start our server
-// server.listen(4000, () => {
-//     console.log(`Server started on port ${server.address().port} :)`);
-// });
-// // const { TelloDrone } = require('yatsw')
-
-// // const tello = new TelloDrone()
-
-// // let ws = new WebSocket.Server({port:4000})
-
-// // // ws.start()
-
-// // tello.start()
-
-// // function sendToTello(commands){
-// //   return "hi"
-// // }
