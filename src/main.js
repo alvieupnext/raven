6689596f64f869e028cd1f6534e11cce859d3453
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-03-31 19:52:13 +0200

    Cleaned up main code

--- src/main.js
+++ src/main.js
@@ -4,55 +4,6 @@ require("./telloserver")
 
 require("./videoFeed")
 
-// const http = require('http');
-// const WebSocket = require('ws');
-
-// const server = http.createServer(app);
-
-// //initialize the WebSocket server instance
-// const wss = new WebSocket.Server({ server });
-
-// const argdict = {
-//     left: [1],
-//     right: [1],
-// }
-
-// const Tello = new TelloDrone()
-
-// wss.on('connection', (ws) => {
-//   Tello.start()
-
-//     //connection is up, let's add a simple simple event
-//     ws.on('message', (message) => {
-
-//         //log the received message and send it back to the client
-//         console.log('received: %s', message);
-//         console.log(message)
-//         console.log(message.toString())
-//         const func = Tello[message.toString()]
-//         const arg = argdict[message.toString()]
-//         console.log(func)
-//         if (arg !== undefined){
-//             func.apply(null, arg)
-//         }
-//         else {func()}
-//         ws.send(`Hello, you sent -> ${message}`);
-//     });
-
-//     //send immediatly a feedback to the incoming connection    
-//     ws.send('Hi there, I am a WebSocket server');
-
-//     ws.on('close', (message) => {
-//       console.log("Closed Websocket")
-//       Tello.destroy()
-//     })
-// });
-
-// //start our server
-// server.listen(4000, () => {
-//     console.log(`Server started on port ${server.address().port} :)`);
-// });
-
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -65,14 +16,6 @@ function createWindow () {
 
   // //load the index.html from a url
   win.loadURL('http://localhost:3000');
-
-  // console.log(__dirname+'/public/')
-
-  // win.loadURL(url.format({
-  //   pathname: path.join(__dirname+'/public/', 'index.html'),
-  //   protocol: 'file:',
-  //   slashes: true
-  // }))
 
   // Open the DevTools.
   win.webContents.openDevTools()
