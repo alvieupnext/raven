c788edd47e9d399bc06e4677719c931737288da0
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-04-11 13:55:22 +0200

    Added remaining sequences

--- src/sequences.js
+++ src/sequences.js
@@ -5,8 +5,20 @@ const seq2 = ["yawCW_180", "flip_b"];
 
 const seq3 = ["sequence_1", "sequence_7"];
 
+const seq4 = ["setSpeed_60", "left_40", "yawCCW_30", "forward_40", "sequence_2"]
+
+const seq5 = []
+
+const seq6 =[]
+
 const seq7 = ["up_30", "down_30"];
 
-const sequences = {1: seq1, 2: seq2, 3: seq3, 7: seq7};
+const seq8 = []
+
+const seq9 = []
+
+const seq10 = []
+
+const sequences = {1: seq1, 2: seq2, 3: seq3, 4: seq4, 5: seq5, 6: seq6, 7: seq7, 8: seq8, 9: seq9, 10: seq10};
 
 module.exports = sequences
