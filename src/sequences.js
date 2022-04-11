8ae5bff8aa8f6e59fd3742b404942ff5ba65d167
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-04-11 12:42:25 +0200

    Created example sequences

---
+++ src/sequences.js
@@ -0,0 +1,10 @@
+// sequence 1, the circle
+const seq1 = ["forward_50", "left_50", "back_50", "right_50"]
+
+const seq2 = ["yawCW_180", "flip_b"]
+
+const seq7 = ["forward_80", "land"]
+
+const sequences = {1: seq1, 2: seq2, 7: seq7}
+
+export {sequences}
