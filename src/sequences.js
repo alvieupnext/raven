86cfa5dcfe758426347a672461fac25367aebc8c
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-04-11 13:02:44 +0200

    Changed sequences

--- src/sequences.js
+++ src/sequences.js
@@ -3,8 +3,10 @@ const seq1 = ["forward_50", "left_50", "back_50", "right_50"]
 
 const seq2 = ["yawCW_180", "flip_b"]
 
-const seq7 = ["forward_80", "land"]
+const seq3 = ["sequence_1", "sequence_7"]
 
-const sequences = {1: seq1, 2: seq2, 7: seq7}
+const seq7 = ["up_30", "down_30"]
+
+const sequences = {1: seq1, 2: seq2, 3: seq3, 7: seq7}
 
 export {sequences}
