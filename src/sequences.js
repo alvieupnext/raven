2cecd28e4501df842ea4a068df9566bdac8e99f3
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-04-11 13:23:56 +0200

    Fixed sequences

--- src/sequences.js
+++ src/sequences.js
@@ -1,12 +1,12 @@
 // sequence 1, the circle
-const seq1 = ["forward_50", "left_50", "back_50", "right_50"]
+const seq1 = ["forward_50", "left_50", "back_50", "right_50"];
 
-const seq2 = ["yawCW_180", "flip_b"]
+const seq2 = ["yawCW_180", "flip_b"];
 
-const seq3 = ["sequence_1", "sequence_7"]
+const seq3 = ["sequence_1", "sequence_7"];
 
-const seq7 = ["up_30", "down_30"]
+const seq7 = ["up_30", "down_30"];
 
-const sequences = {1: seq1, 2: seq2, 3: seq3, 7: seq7}
+const sequences = {1: seq1, 2: seq2, 3: seq3, 7: seq7};
 
-export {sequences}
+module.exports = sequences
